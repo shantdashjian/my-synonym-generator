391953a59420cfefdab023f06a879bdb1a42c443
Shant Dashjian <shantgdashjian@gmail.com>
2024-02-28 21:46:08 +0400

    Move OpenAI API interaction to the backend

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import OpenAI from "openai";
-
 const wordTextrea = document.getElementById('word-textrea')
 const synonymsTextarea = document.getElementById('synonyms-textarea')
 const getSynonymsBtn = document.getElementById('get-synonyms-btn')
@@ -8,31 +6,19 @@ const clearBtn = document.getElementById('clear-btn')
 getSynonymsBtn.addEventListener('click', async () => {
   const wordText = wordTextrea.value
 
-  const openai = new OpenAI({
-    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-    dangerouslyAllowBrowser: true
-  });
-  
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        "role": "system",
-        "content": "You are a linguist ready to answer my questions."
-      },
-      {
-        "role": "user",
-        "content": "Give me 3 synonyms of this word, only giving me the synonyms, without numbering or extra explanantion, sepeareted with commas: " + wordText
-      }
-    ],
-    temperature: 1,
-    max_tokens: 256,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-  });
-
-  synonymsTextarea.value = response.choices[0].message.content
+  const apiUrl = 'http://localhost:3000/api/synonym'
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({
+      'wordText': wordText
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  const response = await fetch(apiUrl, options)
+  const result = await response.json()
+  synonymsTextarea.value = result.synonymsText
 })
 
 clearBtn.addEventListener('click', () => {
